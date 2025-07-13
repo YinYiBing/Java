1d22bf56602979f6dbcf04f9e7a0ce4310f218a9
yinyibing <1980665044@qq.com>
2025-07-13 17:35:27 +0800

    first commit

---
+++ 01_JavaBase/src/main/java/com/yyb/day1/ConstantDemo.java
@@ -0,0 +1,10 @@
+package com.yyb.day1;
+
+public class ConstantDemo {
+    public static void main(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(10 < 20);
+    }
+}
---
+++ 01_JavaBase/src/main/java/com/yyb/day1/IDEADemo.java
@@ -0,0 +1,7 @@
+package com.yyb.day1;
+
+public class IDEADemo {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
